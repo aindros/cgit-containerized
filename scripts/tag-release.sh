e4d90bfcb0f3ee2c891ec525c8ae49b6dc8776bf
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2025-02-26 15:14:52 +0100

    Mark the tag release without copy/paste of commands

--- scripts/tag-release.sh
+++ scripts/tag-release.sh
@@ -5,5 +5,24 @@ if [ ! -z "$(git tag -l | grep $1)" ]; then
     exit
 fi
 
-echo git commit -m '"cgit-containerized '$1'"'
-echo git tag -a v$1 -m '"Version '$1'"'
+COMMIT_MESSAGE='"cgit-containerized '$1'"'
+TAG=v$1
+TAG_MESSAGE='"Version '$1'"'
+
+echo 'Commit message: '$COMMIT_MESSAGE
+echo 'Tag: '$TAG
+echo 'Tag message: '$TAG_MESSAGE
+
+echo
+while [ "$answer" != 'y' ] && [ "$answer" != 'n' ]; do
+	printf "Proceed? [y/n]: "
+	read answer
+done
+
+if [ "$answer" = 'y' ]; then
+	echo git commit -m $COMMIT_MESSAGE
+	git commit -m "cgit-containerized $1"
+
+	echo git tag -a $TAG -m $TAG_MESSAGE
+	git tag -a $TAG -m $TAG_MESSAGE
+fi
